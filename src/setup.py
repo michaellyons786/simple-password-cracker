26a6d10c80bd399817624d7c5197c848d864f398
Michael <mlyonsru@gmail.com>
2018-02-13 14:19:42 -0700

    added setup python script

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='simple password cracker',
+      version='1.0',
+      py_modules=['foo'],
+      author_email='mylonsru@gmail.com',
+      author="Michael Lyons"
+      )
+
